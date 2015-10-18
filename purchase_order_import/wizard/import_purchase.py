257bdfc6eab0967f6b73849dce257e99cbb5df9a
Yoshi Tashiro <tashiro@roomsfor.hk>
2015-10-18 20:20:24 +0800

    [IMP] shorten the main method

--- purchase_order_import/wizard/import_purchase.py
+++ purchase_order_import/wizard/import_purchase.py
@@ -38,6 +38,7 @@ class import_purchase(models.TransientModel):
     datas_fname = fields.Char('File Path')
     supplier_invoice_journal_id = fields.Many2one('account.journal', string='Supplier Invoice Journal', default=_get_supplier_invoice_journal_id)
     supplier_payment_journal_id = fields.Many2one('account.journal', string='Supplier Payment Journal', default=_get_supplier_payment_journal_id)
+
     
     @api.model
     def _get_partner_dict(self, partner_value, partner_dict, error_line_vals):
@@ -70,17 +71,91 @@ class import_purchase(models.TransientModel):
                 pricelist_dict[pricelist_value] = pricelist.id
 
     @api.model
-    def _get_invoice_method(self, invoice_method_value, invoice_method_dict, error_line_vals):
+    def _get_invoice_method(self, invoice_method_value, error_line_vals):
+        invoice_method_dict = {'Based on Purchase Order lines' : 'manual',
+                              'Based on generated draft invoice' : 'order',
+                              'Based on incoming shipments' : 'picking'}
         if not invoice_method_value in invoice_method_dict:
             error_line_vals['error_name'] = error_line_vals['error_name'] + 'Invoice Method: ' + invoice_method_value + ' Not Found! \n'
             error_line_vals['error'] = True
         return invoice_method_dict[invoice_method_value]
     
+    @api.model
+    def _get_picking_dict(self, warehouse_value, picking_dict, error_line_vals):
+        warehouse_id = self.env['stock.warehouse'].search([('name','=',warehouse_value)]).id
+        if not warehouse_id:
+            error_line_vals['error_name'] = error_line_vals['error_name'] + 'Warehouse: ' + warehouse_value + ' Not Found! \n'
+            error_line_vals['error'] = True
+        else:
+            picking_type = self.env['stock.picking.type'].search([('warehouse_id','=',warehouse_id),('code','=','incoming')])
+            picking_dict[warehouse_value] = picking_type
+
+    @api.model
+    def _get_taxes(self, tax_from_chunk, taxes, error_line_vals):
+        tax_name_list = tax_from_chunk.split(',')
+        for tax_name in tax_name_list:
+            tax = self.env['account.tax'].search([('name', '=', tax_name)])
+            if not tax:
+                error_line_vals['error_name'] = error_line_vals['error_name'] + 'Tax: ' + tax_name + ' Not Found! \n'
+                error_line_vals['error'] = True
+            else:
+                taxes.append(tax.id)
+
+    @api.model
+    def _update_error_log(self, error_log_id, error_line_vals, ir_attachment, model, row_no, order_group_value):
+        if not error_log_id and error_line_vals['error']:
+            error_log_id = self.env['error.log'].create({'input_file': ir_attachment.id,
+                                                         'import_user_id' : self.env.user.id,
+                                                        'import_date': datetime.now(),
+                                                        'state': 'failed',
+                                                        'model_id': model.id}).id
+            error_line_id = self.env['error.log.line'].create({
+                                        'row_no' : row_no + 1,
+                                        'order_group' : order_group_value,
+                                        'error_name': error_line_vals['error_name'],
+                                        'log_id' : error_log_id
+                                    })
+        elif error_line_vals['error']:
+            error_line_id = self.env['error.log.line'].create({
+                                        'row_no' : row_no + 1,
+                                        'order_group' : order_group_value,
+                                        'error_name': error_line_vals['error_name'],
+                                        'log_id' : error_log_id
+                                    })
+        return error_log_id
+
+    @api.model
+    def _get_order_id(self, order_data, item):
+        order_vals = {
+            'partner_id' : order_data['partner_id'],
+            'pricelist_id' : order_data['pricelist_id'],
+            'location_id': order_data['location_id'],
+            'invoice_method': order_data['invoice_method'],
+            'imported_order' : True,
+            'order_ref': item,
+            'currency_id': order_data['currency_id'],
+            'date_order' : order_data['date_order'],
+            'notes': order_data['notes'],
+        }
+        return self.env['purchase.order'].create(order_vals)
+
+    @api.model
+    def _get_orderline_id(self, po_line, order_id):
+        orderline_vals = {
+            'name' : po_line['name'],
+            'product_id' : po_line['product_id'],
+            'product_qty' : po_line['product_qty'],
+            'date_planned' : po_line['date_planned'],
+            'price_unit' : po_line['price_unit'],
+            'state' : po_line['state'],
+            'taxes_id': [(6, 0, po_line['taxes_id'])],
+            'order_id' : order_id.id,
+        }
+        return self.env['purchase.order.line'].create(orderline_vals)
+
+
     @api.multi
     def import_purchase_data(self):
-        invoice_method_dict = {'Based on Purchase Order lines' : 'manual',
-                              'Based on generated draft invoice' : 'order',
-                              'Based on incoming shipments' : 'picking'}
         if not self.supplier_invoice_journal_id:
             raise Warning(_('Error!'),_('Please select Supplier Invoice Journal.'))
         
@@ -128,7 +203,7 @@ class import_purchase(models.TransientModel):
                 taxes_id = sheet.row_values(0).index('order_line/taxes_id')
                 partner_id = sheet.row_values(0).index('partner_id')
                 pricelist_id = sheet.row_values(0).index('pricelist_id')
-                picking_type_id = sheet.row_values(0).index('picking_type_id')
+                warehouse_id = sheet.row_values(0).index('Warehouse')
                 invoice_method_name = sheet.row_values(0).index('invoice_method')
                 notes = sheet.row_values(0).index('notes')
 
@@ -148,30 +223,17 @@ class import_purchase(models.TransientModel):
                         self._get_pricelist_dict(pricelist_value, pricelist_dict, error_line_vals)
                     
                     invoice_method_value = sheet.row_values(row_no)[invoice_method_name]
-                    if invoice_method_value:
-                        invoice_method = self._get_invoice_method(invoice_method_value, invoice_method_dict, error_line_vals)
+                    invoice_method = self._get_invoice_method(invoice_method_value, error_line_vals)
                     
-                    picking_type_value = sheet.row_values(row_no)[picking_type_id]
-                    if picking_type_value:
-                        if picking_type_value not in picking_dict:
-                            picking_type = self.env['stock.picking.type'].search([('name', '=', picking_type_value)])
-                            current_pick = False
-                            for pick in picking_type:
-                                if pick.warehouse_id.company_id.id == self.env.user.company_id.id:
-                                    current_pick = pick
-                            picking_dict[picking_type_value] = current_pick
+                    warehouse_value = sheet.row_values(row_no)[warehouse_id]
+                    if warehouse_value:
+                        self._get_picking_dict(warehouse_value, picking_dict, error_line_vals)
                     
                     taxes = []
                     tax_from_chunk = sheet.row_values(row_no)[taxes_id]
                     if tax_from_chunk:
-                        tax_name_list = tax_from_chunk.split(',')
-                        for tax_name in tax_name_list:
-                            tax = self.env['account.tax'].search([('name', '=', tax_name)])
-                            if not tax:
-                                error_line_vals['error_name'] = error_line_vals['error_name'] + 'Tax: ' + tax_name + ' Not Found! \n'
-                                error_line_vals['error'] = True
-                            else:
-                                taxes.append(tax.id)
+                        self._get_taxes(tax_from_chunk, taxes, error_line_vals)
+
                     qty = float(sheet.row_values(row_no)[product_qty])
                     if qty < 0:
                         error_line_vals['error_name'] = error_line_vals['error_name'] + 'Quantity not less then zero! \n'
@@ -182,25 +244,7 @@ class import_purchase(models.TransientModel):
                         error_line_vals['error_name'] = error_line_vals['error_name'] + 'Price Unit not less then zero! \n'
                         error_line_vals['error'] = True
                     
-                    if not error_log_id and error_line_vals['error']:
-                        error_log_id = self.env['error.log'].create({'input_file': ir_attachment.id,
-                                                                     'import_user_id' : self.env.user.id,
-                                                                    'import_date': datetime.now(),
-                                                                    'state': 'failed',
-                                                                    'model_id': model.id}).id
-                        error_line_id = self.env['error.log.line'].create({
-                                                    'row_no' : row_no + 1,
-                                                    'order_group' : order_group_value,
-                                                    'error_name': error_line_vals['error_name'],
-                                                    'log_id' : error_log_id
-                                                })
-                    elif error_line_vals['error']:
-                        error_line_id = self.env['error.log.line'].create({
-                                                    'row_no' : row_no + 1,
-                                                    'order_group' : order_group_value,
-                                                    'error_name': error_line_vals['error_name'],
-                                                    'log_id' : error_log_id
-                                                })
+                    error_log_id = self._update_error_log(error_log_id, error_line_vals, ir_attachment, model, row_no, order_group_value)
                     
                     order = sheet.row_values(row_no)[order_group]
                     if not error_log_id:
@@ -240,7 +284,7 @@ class import_purchase(models.TransientModel):
                             order_dict[order_group_value] = {
                                             'partner_id' : partner_dict[partner_value],
                                             'pricelist_id' : pricelist_dict[pricelist_value],
-                                            'location_id': picking_dict[picking_type_value].default_location_dest_id and picking_dict[picking_type_value].default_location_dest_id.id,
+                                            'location_id': picking_dict[warehouse_value].default_location_dest_id and picking_dict[warehouse_value].default_location_dest_id.id,
                                             'invoice_method': invoice_method,
                                             'currency_id' : pricelist_data['value']['currency_id'],
                                             'date_order' : planned_date,
@@ -255,40 +299,31 @@ class import_purchase(models.TransientModel):
                                                                  'model_id': model.id}).id
                                                                     
                     for item in order_item_dict:
-                        order_data = order_dict[item]
-                        order_vals = {
-                            'partner_id' : order_data['partner_id'],
-                            'pricelist_id' : order_data['pricelist_id'],
-                            'location_id': order_data['location_id'],
-                            'invoice_method': order_data['invoice_method'],
-                            'imported_order' : True,
-                            'order_ref': item,
-                            'currency_id': order_data['currency_id'],
-                            'date_order' : order_data['date_order'],
-                            'notes': order_data['notes'],
-                        }
-                        order_id = self.env['purchase.order'].create(order_vals)
+                        order_id = self._get_order_id(order_dict[item], item)
+                        
                         for po_line in order_item_dict[item]:
-                            orderline_vals = {
-                                'name' : po_line['name'],
-                                'product_id' : po_line['product_id'],
-                                'product_qty' : po_line['product_qty'],
-                                'date_planned' : po_line['date_planned'],
-                                'price_unit' : po_line['price_unit'],
-                                'state' : po_line['state'],
-                                'taxes_id': [(6, 0, po_line['taxes_id'])],
-                                'order_id' : order_id.id,
-                            }
-                            orderline_id = self.env['purchase.order.line'].create(orderline_vals)
+                            orderline_id = self._get_orderline_id(po_line, order_id)
+
                         order_id.signal_workflow('purchase_confirm')
+                        
                         if order_id.invoice_ids:
                             for invoice in order_id.invoice_ids:
                                 invoice.journal_id = self.supplier_invoice_journal_id.id
                                 if invoice.state == 'draft':
                                     invoice.signal_workflow('invoice_open')
-                                    journal_data = self.env['account.voucher'].onchange_journal_voucher(line_ids= False, tax_id=False, price=0.0, partner_id=invoice.partner_id.id, journal_id=invoice.journal_id.id, ttype=False, company_id=invoice.company_id.id)
-                                    partner_data  = self.env['account.voucher'].onchange_partner_id(invoice.partner_id.id, 
-                                                                                                invoice.journal_id.id, invoice.amount_total , invoice.currency_id.id, 'payment', invoice.date_invoice)
+                                    journal_data = self.env['account.voucher'].onchange_journal_voucher(line_ids= False,
+                                                                                                        tax_id=False,
+                                                                                                        price=0.0, 
+                                                                                                        partner_id=invoice.partner_id.id,
+                                                                                                        journal_id=invoice.journal_id.id,
+                                                                                                        ttype=False,
+                                                                                                        company_id=invoice.company_id.id)
+                                    partner_data  = self.env['account.voucher'].onchange_partner_id(invoice.partner_id.id,
+                                                                                                    invoice.journal_id.id,
+                                                                                                    invoice.amount_total,
+                                                                                                    invoice.currency_id.id,
+                                                                                                    'payment',
+                                                                                                    invoice.date_invoice)
                                     line_dr_list = []
                                     for line in partner_data['value']['line_dr_ids']:
                                         moveline = self.env['account.move.line'].browse(line['move_line_id'])
@@ -313,9 +348,9 @@ class import_purchase(models.TransientModel):
                                     voucher_id = self.env['account.voucher'].create(voucher_vals)
                                     voucher_id.signal_workflow('proforma_voucher')
                          
-            result = self.env.ref('base_import_log.error_log_action')
-            result = result.read()[0]
-            result['domain'] = str([('id','in',[error_log_id])])
-            return result
+            res = self.env.ref('base_import_log.error_log_action')
+            res = res.read()[0]
+            res['domain'] = str([('id','in',[error_log_id])])
+            return res
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
