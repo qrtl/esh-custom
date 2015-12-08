0443f58a6dde943f40bddf0add4e55bb0c1dbc32
manamirfhk <manami@roomsfor.hk>
2015-12-08 17:32:06 +0800

    [UPD] update depend and translation

--- purchase_line_original_postage/__openerp__.py
+++ purchase_line_original_postage/__openerp__.py
@@ -32,7 +32,7 @@
 * Adds above columns in the report of Invoice
 * Adds Variation and Comments fields in Purchase Order Lines
     """,
-    'depends': ["purchase", "account"], 
+    'depends': ["purchase", "account_invoice"], 
     'data': [
         'views/purchase_view.xml',
         'views/account_invoice_view.xml',
