240968f5242aef0eacb15cc4179472c1c98ba9ab
manamirfhk <manami@roomsfor.hk>
2015-12-09 19:41:57 +0800

    [ADD] add module

---
+++ web_url/__openerp__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Web/URL widget",
+    "description":
+        """
+        After Installation of this module, user will be directly redirect to given URL, if field's widget set to widget="url" in tree view.
+        
+        You can send feedback to info@emiprotechnologies.com,
+ 
+        ---
+
+        This module is originally for V7 and migrated for V8.
+
+        """,
+    "version": "8.0",
+    "author" : "Emipro Technologies, Rooms For (Hong Kong) Limited T/A OSCG",
+    "website" : "http://www.emiprotechnologies.com",
+    "category" : "Tools",
+    "depends" : ["web"],
+    "data": [
+        "template.xml",
+    ],
+    "auto_install": False,
+}
