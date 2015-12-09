cee45642970390d9de52f625fbe45b4a659e7144
manamirfhk <manami@roomsfor.hk>
2015-12-09 12:20:07 +0800

    [ADD] create module

--- purchase_status_message/models/purchase.py
+++ purchase_status_message/models/purchase.py
@@ -21,9 +21,19 @@
 ##############################################################################
 
 from openerp.osv import fields, osv
+import openerp.addons.decimal_precision as dp
 
 class purchase_order(osv.osv):
     _inherit = "purchase.order"
+
+    def _amount_all(self, cr, uid, ids, field_name, arg, context=None):
+        return super(purchase_order, self)._amount_all(cr, uid, ids, field_name, arg, context)
+
+    def _get_order(self, cr, uid, ids, context=None):
+        result = {}
+        for line in self.pool.get('purchase.order.line').browse(cr, uid, ids, context=context):
+            result[line.order_id.id] = True
+        return result.keys()
 
     READONLY_STATES = {
         'confirmed': [('readonly', True)],
@@ -33,21 +43,25 @@ class purchase_order(osv.osv):
 
     _track = {
         'state': {
-            'purchase_bid_received_message.mt_rfq_draft': lambda self, cr, uid, obj, ctx=None: obj.state == 'draft',
-            'purchase_bid_received_message.mt_rfq_sent': lambda self, cr, uid, obj, ctx=None: obj.state == 'sent',
-            'purchase_bid_received_message.mt_rfq_bid': lambda self, cr, uid, obj, ctx=None: obj.state == 'bid',
-            'purchase_bid_received_message.mt_rfq_confirmed': lambda self, cr, uid, obj, ctx=None: obj.state == 'confirmed',
-            'purchase_bid_received_message.mt_rfq_approved': lambda self, cr, uid, obj, ctx=None: obj.state == 'approved',
-            'purchase_bid_received_message.mt_rfq_except_picking': lambda self, cr, uid, obj, ctx=None: obj.state == 'except_picking',
-            'purchase_bid_received_message.mt_rfq_except_invoice': lambda self, cr, uid, obj, ctx=None: obj.state == 'except_invoice',
-            'purchase_bid_received_message.mt_rfq_done': lambda self, cr, uid, obj, ctx=None: obj.state == 'done',
-            'purchase_bid_received_message.mt_rfq_cancel': lambda self, cr, uid, obj, ctx=None: obj.state == 'cancel',
+            'purchase_status_message.mt_rfq_draft': lambda self, cr, uid, obj, ctx=None: obj.state == 'draft',
+            'purchase_status_message.mt_rfq_sent': lambda self, cr, uid, obj, ctx=None: obj.state == 'sent',
+            'purchase_status_message.mt_rfq_bid': lambda self, cr, uid, obj, ctx=None: obj.state == 'bid',
+            'purchase_status_message.mt_rfq_confirmed': lambda self, cr, uid, obj, ctx=None: obj.state == 'confirmed',
+            'purchase_status_message.mt_rfq_approved': lambda self, cr, uid, obj, ctx=None: obj.state == 'approved',
+            'purchase_status_message.mt_rfq_except_picking': lambda self, cr, uid, obj, ctx=None: obj.state == 'except_picking',
+            'purchase_status_message.mt_rfq_except_invoice': lambda self, cr, uid, obj, ctx=None: obj.state == 'except_invoice',
+            'purchase_status_message.mt_rfq_done': lambda self, cr, uid, obj, ctx=None: obj.state == 'done',
+            'purchase_status_message.mt_rfq_cancel': lambda self, cr, uid, obj, ctx=None: obj.state == 'cancel',
         },
     }
 
     _columns = {
         'partner_id':fields.many2one('res.partner', 'Supplier', required=True, states=READONLY_STATES,
             change_default=True, track_visibility='false'), 
+        'amount_untaxed': fields.function(_amount_all, digits_compute=dp.get_precision('Account'), string='Untaxed Amount',
+            store={
+                'purchase.order.line': (_get_order, None, 10),
+            }, multi="sums", help="The amount without tax", track_visibility='false'),
     }
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
