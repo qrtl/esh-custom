0f91af273de026ee28258b92cdcd7c9a89b44c07
manamirfhk <manami@roomsfor.hk>
2016-01-05 16:50:59 +0800

    [ADD] create module

---
+++ account_supplier_payment_adjust/__openerp__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2015 Rooms For (Hong Kong) Limited T/A OSCG
+#    <https://www.odoo-asia.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+{
+    'name': 'Account Voucher Adjustment for EarthShip',
+    'version': '8.0.0.5',
+    'author': 'Rooms For (Hong Kong) T/A OSCG',
+    'depends': ['account_voucher', 'legroup_purchase_warehouse'],
+    'website': 'https://www.odoo-asia.com',
+    'summary':"""""",
+    'description': """ 
+Adjust Screen of Account Voucher
+    """,
+    'data': [
+            'views/voucher_payment_receipt_view.xml',
+             ],
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
